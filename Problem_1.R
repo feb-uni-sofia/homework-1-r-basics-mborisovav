0711af637e7d1d21b0c228ae32f4542ff17ddd0a
Mariya Vasileva <36989384+mborisovav@users.noreply.github.com>
2018-04-02 01:47:27 +0300

    Add files via upload

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,32 @@
-
+> x <- c(4,1,1,4)
+> x
+[1] 4 1 1 4
+> # a) created a vector called x
+  > # b) create a vector called y
+  > y <- c(1,4)
+  > y
+  [1] 1 4
+  > # difference between x and y 
+    > x - y
+  [1]  3 -3  0  0
+  > # because of the different lenght of the vectors. the parametres from y are used twice so the command to be fullfilled
+    > # create a new vector z
+    > z <- c (x,y)
+    > z
+    [1] 4 1 1 4 1 4
+    > # repeat S 10 time
+      > rep(z, 10)
+    [1] 4 1 1 4 1 4 4 1 1 4 1 4 4 1 1 4 1 4 4 1 1 4 1 4 4 1 1 4 1 4 4 1 1 4 1 4 4 1 1
+    [40] 4 1 4 4 1 1 4 1 4 4 1 1 4 1 4 4 1 1 4 1 4
+    > length(rep(z, 10))
+    [1] 60
+    > # repeat each element three times
+      > rep(z, each = 3)
+    [1] 4 4 4 1 1 1 1 1 1 4 4 4 1 1 1 4 4 4
+    > # create a sequence from 7 to 21
+      > seq(7,21)
+    [1]  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21
+    > # the lenght of the sequence
+      > length(seq(7,21))
+    [1] 15
+    
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,31 @@
+# create vectors with xmin and xmax
+xmin <- c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+xmax <- c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+# the difference between max and min every day
+xmax - xmin
+# because the vectors are symethrical that shows the diffenrence for each day
+# the average of min and the average of max
+mean(xmax)
+mean(xmin)
+# elements from xmin which are < 21.4
+d <- xmin[xmin < 21.4]
+# elements from xmin which are > 25.57
+e <- xmin[xmin > 25.57]
+# name the elements of the vectors
+names(xmax) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+names(xmin) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+# create a data frame from xmin and xmax
+temparatures <- data.frame(xmax, xmin)
+#create a new column xminFahrenheit which is xmin in Farenheit
+temparatures ["xminFahrenhei"] <- NA
+temparatures$xminFahrenhei <- temparatures$xmin*1.8 + 32
+# create a new data frame only with Far measurements
+seconddata <- data.frame(temparatures$xminFahrenhei)
+# create a new data frame including the measurement for the first five days
+thirddate <- data.frame(seconddata$temparatures.xminFahrenhei[1 : 5])
+thirddate <- data.frame(seconddata$temparatures.xminFahrenhei [c(-6,-7)])
+
+
+
+
 
